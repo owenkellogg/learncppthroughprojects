2427d2a5d805f8e1bd70ef6237a9019ee7c49376
My CTO <mycto@proton.me>
2025-07-24 21:33:55 +0200

    Convert the code into a static library

---
+++ tests/websocket-client.cpp
@@ -0,0 +1,19 @@
+#include <network-monitor/websocket-client.h>
+
+#include <boost/asio.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+using NetworkMonitor::WebSocketClient;
+
+BOOST_AUTO_TEST_SUITE(network_monitor);
+
+BOOST_AUTO_TEST_CASE(class_WebSocketClient)
+{
+
+    // Connection targets
+    // ...
+}
+
+BOOST_AUTO_TEST_SUITE_END();
